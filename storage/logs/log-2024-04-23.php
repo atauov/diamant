1696736636fbc58a952b28ef00f069a8fbefcaa5
atauov <atauov@gmail.com>
2024-04-23 08:38:58 +0000

    first commit

---
+++ storage/logs/log-2024-04-23.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-04-23 04:22:38 --> Severity: Warning --> mysqli::real_connect(): (HY000/1044): Access denied for user 'easy_appointments_rw'@'localhost' to database 'branch1' /var/www/html/branch1/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2024-04-23 04:22:38 --> Severity: error --> Exception: Unable to connect to the database. /var/www/html/branch1/system/database/DB_driver.php 433
