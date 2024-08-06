9d9b4a589034c6d744ad391c80287141239120b3
atauov <atauov@gmail.com>
2024-08-06 20:49:42 +0500

    try

--- application/views/backend/calendar.php
+++ application/views/backend/calendar.php
@@ -317,7 +317,7 @@
                                         <span class="text-danger">*</span>
                                     </label>
                                     <input type="hidden" name="email" value="customer@diamant.com">
-                                    <p class="required form-control">customer@diamant.com</p>                                </div>
+                                        </div>
 
                                 <div class="form-group">
                                     <label for="phone-number" class="control-label">
